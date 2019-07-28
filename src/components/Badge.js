ca8c4ff489ab3e958f031dda2f101c570ed804fb
Tekorita <davidpalmalugo@gmail.com>
2019-07-27 21:56:12 -0400

    nueva pagina badgenew con componentes nav y badge

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import "../styles/Badge.css"
+import "./styles/Badge.css"
 import confLogo from '../images/badge-header.svg'
 //Los componentes son clases
 
--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,11 +1,31 @@
 import React from 'react'
+import './styles/BadgeNew.css'
+import header from '../images/badge-header.svg'
+import Navbar from '../components/Navbar'
+import Badge from '../components/Badge'
 
 class BadgeNew extends React.Component {
     render(){
         return (
             <div>
                 <Navbar />
-            </div>
+                <div className="BadgeNew__hero">
+                    <img className="img-fluid" src={header} alt="Logo" /> 
+                </div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                        <Badge 
+                            firstName="David" 
+                            lastName="Palma" 
+                            avatarUrl="https://firebasestorage.googleapis.com/v0/b/davidpalmalugo-6ffa4.appspot.com/o/yo3.jpg?alt=media&token=b80bc1df-ea46-4595-abfc-a692be4b2f90"
+                            jobTitle="Fronted Engineer" 
+                            twitter="@Davidpalmalugo"                  
+                            />  
+                        </div>
+                    </div>
+                </div>
+            </div>       
         )
     }
 }
