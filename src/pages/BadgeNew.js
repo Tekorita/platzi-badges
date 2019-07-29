1699f135a2c779014890a7d47734c8541d10c071
Tekorita <davidpalmalugo@gmail.com>
2019-07-29 04:11:08 -0400

    refactor

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import './styles/BadgeNew.css'
 import header from '../images/badge-header.svg'
-import Navbar from '../components/Navbar'
 import Badge from '../components/Badge'
 import BadgeForm from '../components/BadgeForm'
 
@@ -40,7 +39,6 @@ class BadgeNew extends React.Component {
     render(){
         return (
             <div>
-                <Navbar />
                 <div className="BadgeNew__hero">
                     <img className="img-fluid" src={header} alt="Logo" /> 
                 </div>
