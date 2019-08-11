7bca6ea50380e42e6156a355f074b3cc20ae8ee4
Tekorita <davidpalmalugo@gmail.com>
2019-08-10 23:56:13 -0400

    correccion en badge 3

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -20,7 +20,7 @@ class Badge extends React.Component {
                     <div>{this.props.twitter}</div>
                 </div>
                 <div className="Badge__footer">
-                    <p>Platzi conferenc</p>
+                    <p>Platzi conf</p>
                 </div>
             </div>
         )
