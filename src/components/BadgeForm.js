cd4066b268e7fb79e7c81615d86a4070e8d1f7ba
Tekorita <davidpalmalugo@gmail.com>
2019-08-27 23:16:02 -0400

    update form list badges social network

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -73,7 +73,7 @@ class BadgeForm extends React.Component {
                             value={this.props.formValues.jobTitle} />                    
                     </div>
                     <div className="form-group">
-                        <label>Twitter or face</label>
+                        <label>Twitter or face o redes sociales</label>
                         <input 
                             onChange = {this.props.onChange} 
                             className="form-control" 
